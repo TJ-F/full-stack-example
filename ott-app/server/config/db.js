55493199855c3cfc4aa6a90e6d8debeb800fb8d1
TJ Foster <50240280+TJ-F@users.noreply.github.com>
2026-02-01 14:55:05 -0500

    Refactor DB connection to use connection pool

--- ott-app/server/config/db.js
+++ ott-app/server/config/db.js
@@ -1,35 +1,33 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-import fs from 'fs';
-import path from 'path';
 
 dotenv.config();
-// Azure MySQL connection example
 
-// var fs = require('fs');
-// var mysql = require('mysql');
-// const serverCa = [fs.readFileSync("/var/www/html/DigiCertGlobalRootCA.crt.pem", "utf8")];
+let conn;
 
-export async function createDbConnection() {
-  const isLocalhost = process.env.DB_HOST === 'localhost' || process.env.DB_HOST === '127.0.0.1';
+export function createDbConnection() {
+  if (!conn) {
+    if (!process.env.DB_CA_CERT) {
+      throw new Error('DB_CA_CERT is required for Aiven MySQL');
+    }
 
-  const conn = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT) || 3306,
-    ssl: isLocalhost
-      ? false
-      : {
-          rejectUnauthorized: true,
-          ca: fs.readFileSync(path.resolve('./certs/DigiCertGlobalRootG2.crt.pem')),
-        },
-    connectTimeout: 10000,
-    maxIdle: 0,
-    idleTimeout: 60000,
-    enableKeepAlive: true,
-  });
+    conn = mysql.createPool({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+
+      connectionLimit: 5,
+      waitForConnections: true,
+      queueLimit: 0,
+
+      ssl: {
+        rejectUnauthorized: true,
+        ca: process.env.DB_CA_CERT
+      }
+    });
+  }
 
   return conn;
 }
